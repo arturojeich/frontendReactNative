85a53291d18af529ecb18ef5f6a3015f7213530b
arturojeich <arturojeich@gmail.com>
2023-10-25 12:13:05 -0300

    Cleaning up

--- src/screens/appointments/Appointments.js
+++ src/screens/appointments/Appointments.js
@@ -1,21 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { SafeAreaView, Text, StyleSheet, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import Tabs from '../../components/Tabs'
-import { ListAppointments } from './ListAppointments'
-import { CreateAppointment } from './CreateAppointment'
-import { MaterialIcons } from '@expo/vector-icons'
-
-// name={'TabOne'}
-//         options={{
-//           tabBarIcon: ({ focused }) => (
-//             <Feather
-//               name={'droplet'}
-//               size={25}
-//               color={focused ? 'black' : 'grey'}
-//             />
-//           )
-//         }}
 
 const Appointments = () => {
   const listTabs = [
