93be221716162b99e32d3247de9e892764b6b5e4
arturojeich <arturojeich@gmail.com>
2023-10-27 16:18:39 -0300

    Card view for List People

---
+++ src/components/PeopleItem.js
@@ -0,0 +1,141 @@
+import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+import { CustomStyles } from '../customStyles/CustomStyles'
+import { confirm } from './Alerts'
+
+const PeopleItem = ({
+  peopleData,
+  id,
+  deleteFunction,
+  putFunction,
+  getFunction,
+  navigation
+}) => {
+  const {
+    container,
+    titleTheme,
+    textTheme,
+    doctorIcon,
+    buttons,
+    shadowProp,
+    data,
+    elevation
+  } = styles
+  const { nombre, apellido, telefono, cedula, email, es_doctor } = peopleData
+  return (
+    <View key={id} style={[container, shadowProp, elevation]}>
+      <View style={[data]}>
+        <Text
+          style={[titleTheme, { alignSelf: 'flex-start' }]}
+        >{`${nombre} ${apellido}`}</Text>
+        <Text style={[titleTheme]}>CI </Text>
+        <Text style={[textTheme]}>{cedula}</Text>
+        <Text style={[titleTheme]}>Email</Text>
+        <Text style={[textTheme]}>{email}</Text>
+        <Text style={[titleTheme]}>Telefono</Text>
+        <Text style={[textTheme]}>{telefono}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={[titleTheme]}>Doctor:</Text>
+          {es_doctor ? (
+            <View style={doctorIcon}>
+              <MaterialIcons
+                name="check-circle-outline"
+                size={30}
+                color="green"
+              />
+            </View>
+          ) : (
+            <View style={doctorIcon}>
+              <MaterialIcons name="close" size={30} color="grey" />
+            </View>
+          )}
+        </View>
+      </View>
+      <View style={[buttons]}>
+        <MaterialIcons.Button
+          name="edit"
+          size={26}
+          color="black"
+          style={{ marginRight: 0, marginLeft: 0 }}
+          backgroundColor={CustomStyles.colors.mainCard}
+          onPress={() => {
+            navigation.navigate('Editar Persona', {
+              id: id,
+              peopleData: peopleData
+            })
+          }}
+        />
+        <MaterialIcons.Button
+          name="delete"
+          size={26}
+          color="black"
+          backgroundColor={CustomStyles.colors.mainCard}
+          onPress={() =>
+            confirm(
+              'Eliminar',
+              `Esta seguro de eliminar el registro de ${nombre} ${apellido}?`,
+              deleteFunction,
+              id
+            )
+          }
+        />
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 300,
+    width: '100%',
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    paddingRight: 10,
+    backgroundColor: CustomStyles.colors.mainCard
+  },
+  titleTheme: {
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 20
+  },
+  textTheme: {
+    color: 'black',
+    fontSize: 16
+  },
+  doctorIcon: {
+    width: 40,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  },
+  data: {
+    justifyContent: 'center',
+    height: '100%',
+    width: '80%',
+    paddingLeft: 10,
+    borderTopLeftRadius: 10,
+    borderBottomLeftRadius: 10
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '20%',
+    height: '100%',
+    marginRight: 10,
+    borderTopRightRadius: 10,
+    borderBottomRightRadius: 10
+  },
+  shadowProp: {
+    shadowColor: 'black'
+  },
+  elevation: {
+    elevation: 5
+  }
+})
+export default PeopleItem
--- src/screens/people/People.js
+++ src/screens/people/People.js
@@ -1,20 +1,167 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  ScrollView,
+  View,
+  ActivityIndicator,
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet
+} from 'react-native'
+import { app } from '../../../firebaseConfig'
+import {
+  getDatabase,
+  ref,
+  onValue,
+  push,
+  update,
+  remove
+} from 'firebase/database'
 import { NavigationContainer } from '@react-navigation/native'
-import Tabs from '../../components/Tabs'
+import { createStackNavigator } from '@react-navigation/stack'
+import CreatePeople from './CreatePeople'
+import PeopleItem from '../../components/PeopleItem'
+import { CustomStyles } from '../../customStyles/CustomStyles'
+
+var db = null
+
+const ListPeople = ({ navigation }) => {
+  const [peopleList, setPeopleList] = useState({})
+  const peopleKeys = Object.keys(peopleList)
+
+  db = getDatabase(app)
+  useEffect(() => {
+    return onValue(ref(db, '/administracion/personas'), (querySnapShot) => {
+      let data = querySnapShot.val() || {}
+      let itemList = { ...data }
+      setPeopleList(itemList)
+    })
+  }, [])
+
+  function GetAllPeople() {
+    return (
+      <ScrollView>
+        <View>
+          {peopleKeys.length > 0 ? (
+            peopleKeys.map((key) => (
+              <PeopleItem
+                key={key}
+                peopleData={peopleList[key]}
+                id={key}
+                deleteFunction={deletePeople}
+                getFunction={getPeople}
+                navigation={navigation}
+              />
+            ))
+          ) : (
+            <ActivityIndicator size={'large'} color={`black`} />
+          )}
+        </View>
+      </ScrollView>
+    )
+  }
+
+  function FooterButtons() {
+    return (
+      <TouchableOpacity
+        style={CustomStyles.createButton}
+        onPress={() => navigation.navigate('Agregar Persona')}
+      >
+        <Text style={CustomStyles.createButtonText}>Nueva Persona</Text>
+      </TouchableOpacity>
+    )
+  }
+
+  function getPeople() {
+    console.log('Get People')
+  }
+
+  function deletePeople(key) {
+    console.log('Delete people, with KEY: ' + key)
+    console.log(peopleList[key])
+    remove(ref(db, `/administracion/personas/${key}`))
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <GetAllPeople />
+      <FooterButtons />
+    </SafeAreaView>
+  )
+
+  // const listTabs = [
+  //   {
+  //     key: 'List People'
+  //   },
+  //   {
+  //     key: 'Create People'
+  //   }
+  // ]
+  // return (
+  //   <NavigationContainer independent={true}>
+  //     <Tabs listTabs={listTabs} />
+  //   </NavigationContainer>
+  // )
+}
+
+const forFade = ({ current, next }) => {
+  const opacity = Animated.add(
+    current.progress,
+    next ? next.progress : 0
+  ).interpolate({
+    inputRange: [0, 1, 2],
+    outputRange: [0, 1, 0]
+  })
+
+  return {
+    leftButtonStyle: { opacity },
+    rightButtonStyle: { opacity },
+    titleStyle: { opacity },
+    backgroundStyle: { opacity }
+  }
+}
+
+const Stack = createStackNavigator()
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Lista de Personas"
+        component={ListPeople}
+        options={{
+          headerTintColor: CustomStyles.colors.mainText,
+          headerStyle: { backgroundColor: 'white' },
+          headerTitleAlign: 'center'
+        }}
+      />
+      <Stack.Screen
+        name="Agregar Persona"
+        options={{
+          headerStyleInterpolator: forFade,
+          headerTitleAlign: 'center'
+        }}
+      >
+        {(props) => <CreatePeople {...props} db={db} />}
+      </Stack.Screen>
+      {/* <Stack.Screen
+        name="Editar Persona"
+        options={{
+          headerTintColor: CustomStyles.colors.mainText,
+          headerStyle: { backgroundColor: 'white' },
+          headerTitleAlign: 'center'
+        }}
+      >
+        {(props) => <EditPeople {...props} db={db} />}
+      </Stack.Screen> */}
+    </Stack.Navigator>
+  )
+}
 
 const People = () => {
-  const listTabs = [
-    {
-      key: 'List People'
-    },
-    {
-      key: 'Create People'
-    }
-  ]
   return (
     <NavigationContainer independent={true}>
-      <Tabs listTabs={listTabs} />
+      <MyStack />
     </NavigationContainer>
   )
 }
